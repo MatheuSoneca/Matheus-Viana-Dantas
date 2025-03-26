d530b5b084edfccc40e3dbbb96c5c1a15acb8337
MatheuSoneca <matheusvianad@hotmail.com>
2025-03-26 17:18:34 -0300

    some changes in the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 
+
 function App() {
 
 
@@ -7,12 +8,19 @@ function App() {
     <>
       <header>
         <div className='container'>
-          <h1>Matheus Viana Dantas</h1>
-
+          <div className='menu'>
+            <h1>Matheus Viana Dantas</h1>
+            <button>tema</button>
+            <button>language</button>
+          </div>
           <div className='subtitle'>
-            <h2>About me</h2>
-            <h2>Technologies & Tools</h2>
-            <h2>Projects</h2>
+            <nav>
+              <ul className="listaHorizontal">
+                <li><a href="#about">About me</a></li>
+                <li><a href="#technologies">Technologies & Tools</a></li>
+                <li><a href="#projects">Projects</a></li>
+              </ul>
+            </nav>
           </div>
         </div>
 
@@ -20,15 +28,15 @@ function App() {
 
       <main>
         <div className='card'>
-          <div className='aboutMe'>
+          <div id='about'>
             <h2>About me</h2>
             <p>Hello! My name is Matheus Viana Dantas, I am 23 years old and I live in Montes Claros, MG, Brazil.</p>
             <p>I am currently studying Systems Development and have knowledge in JavaScript, TypeScript, and React. I am also learning Node.js and have explored Python. Although I do not have professional experience yet, I have been improving my skills through personal projects and continuous learning.</p>
           </div>
         </div>
 
-        <div className='techs'>
-          <h2>Technologies & Tools</h2>
+        <div className='card'>
+          <h2 id='technologies'>Technologies & Tools</h2>
           <strong>Languages:</strong> JavaScript, TypeScript, Python (basic) <br />
           <strong>Front-end:</strong> React, MUI <br />
           <strong>Back-end:</strong> Node.js, Express <br />
@@ -36,7 +44,7 @@ function App() {
         </div>
 
         <div className='projects'>
-          <h2>Projects</h2>
+          <h2 id='projects'>Projects</h2>
           <div className='projectsCard'>
 
           </div>
